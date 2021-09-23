b3d28c8c11ebbbf7f9b706cbe3dc1d642c47d7b0
sho135 <sh135pro@gmail.com>
2021-09-23 23:14:43 +0000

    edit

--- db/migrate/20210916071640_add_columns_to_books.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToBooks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :books, :title, :string
-    
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_071640) do
+ActiveRecord::Schema.define(version: 2021_09_14_110538) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
   end
 
 end
